91fd2e02cd5ab45953a0380b18e5656554a71cb2
lynnswap <65545348+lynnswap@users.noreply.github.com>
2025-06-30 16:26:19 +0900

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PDJSKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PDJSKit",
+            targets: ["PDJSKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PDJSKit"),
+        .testTarget(
+            name: "PDJSKitTests",
+            dependencies: ["PDJSKit"]
+        ),
+    ]
+)
